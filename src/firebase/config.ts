2865f9db99f9d3735433fb0fbde63941b7e4ce95
T Jai Surya Prasad <suryajai642@gmail.com>
2026-03-05 10:29:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1573377387-ec6ab",
+  "appId": "1:403777988724:web:4df70c13a9a3a93be44921",
+  "apiKey": "AIzaSyDpQgbYVbscFW9Xf7SrNJ7WUTQGNMgPlsU",
+  "authDomain": "studio-1573377387-ec6ab.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "403777988724"
+};
